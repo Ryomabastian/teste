9e56b2a2d2576917459ee320a97c70d1e3621ac3
Ryomabastian <daniel.bastianelle@gmail.com>
2022-12-07 23:08:11 +0000

    oi

--- teste.js
+++ teste.js
@@ -1,8 +1,1 @@
-const prompt = require('prompt-sync')();
-//
-// get input from the user.
-//
-const n = prompt("How many more times? " );
-
-const resultado = n*n
-console.log(`o resutlado é ${resultado}`)
+console.log ("oi")
